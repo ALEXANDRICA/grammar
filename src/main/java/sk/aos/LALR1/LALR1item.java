2dbe29737c1eb81c9941fe3df667c63d9d1f3924
Alexandra Javorska <alexyawor@gmail.com>
2022-12-13 21:29:38 +0100

    LALR1item/vytvorenie poloziek + ukazovatele

--- src/main/java/sk/aos/LALR1/LALR1item.java
+++ src/main/java/sk/aos/LALR1/LALR1item.java
@@ -1,4 +1,36 @@
 package sk.aos.LALR1;
 
+import sk.aos.grammar.Rule;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class LALR1item {
+
+    Rule LALRrule;											// atribut pre LALR1 pravidlo s "." ako ukazovatel
+    HashSet<String> expectedSymbols = new HashSet<>();		// atribut reprezentujuci ocakavany symbol pre LALR1 polozku
+
+	public LALR1item(Rule rule, HashSet<String> expectSymbols){    // parametre su pravidla G, z kt. chceme vytvorit LALR1 polozky v jednotl. stavoch; + ocakavane symboly
+        ArrayList<String> rightSide = new ArrayList<>(rule.getRightSide());
+        ArrayList<String> leftSide = new ArrayList<>(rule.getLeftSide());
+
+        if(rightSide.contains(".")) {							// vytvorenie novej polozky s ukazovatelom "." na cast pravidla, ktora uz bola spracovana v zasobniku
+            int i = rightSide.indexOf(".");
+            rightSide.set(i, rule.getRightSide().get(i+1));
+            rightSide.set(i+1, ".");
+        } else {                                                // else vytvorenie novej polozky LALR1 s ukazovatelom "." na zaciatku
+            int size = rightSide.size();
+            rightSide.add(rightSide.get(size-1));
+            for(int i = size-1; i > 0; i--) {
+                rightSide.set(i, rightSide.get(i-1));
+            }
+            rightSide.set(0, ".");
+        }
+
+        this.LALRrule = new Rule(leftSide, rightSide);
+        this.expectedSymbols = expectSymbols;
+    }
+
+    // dokoncit
 }
+
